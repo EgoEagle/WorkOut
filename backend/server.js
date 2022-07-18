3f597ced8c791ac2c888edb450ac45c049c7d01a
Tony <TonyLin3591@gmail.com>
2022-07-18 00:48:50 -0400

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
+require('dotenv').config()
+
 const express = require('express')
 
+
+//express
 const app = express()
 
-app.listen(4000, () => ){
+
+//routes
+app.get('/',(req, res) => {
+     res.json({mssg: 'Welcome 3'})
+
+})
+
+
+app.listen(process.env.PORT, () => {
      console.log('listening on port 4000')
 
 })
